4faf408940894fe2feaf8ff6fa5af752aa9c75e5
Judyanna Ladella <jladella@ucsc.edu>
2026-03-11 17:42:45 -0700

    changed module name error

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_judyladella_pixel_scan (
+  tt_um_judyladella_pixel_scan user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST

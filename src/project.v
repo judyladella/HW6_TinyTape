bf2361b5763e0fdb476a8afc69cd8a09a8ecaac8
Judyanna Ladella <jladella@ucsc.edu>
2026-03-11 17:52:15 -0700

    add USE_POWER_PINS support for gate-level sim

--- src/project.v
+++ src/project.v
@@ -6,6 +6,10 @@
 `default_nettype none
 
 module tt_um_judyladella_pixel_scan (
+`ifdef USE_POWER_PINS
+    input VPWR,
+    input VGND,
+`endif
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,    // Dedicated outputs
     input  wire [7:0] uio_in,    // IOs: Input path
